53780f69dac70c9c2915c9434a385d65fd8a3f3b
Angel Motta <angel.motta@utec.edu.pe>
2023-07-02 23:20:20 -0500

    feat: init project

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './routes/Login.tsx';
+import Signup from './routes/Signup.tsx';
+import Dashboard from './routes/Dashboard.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
